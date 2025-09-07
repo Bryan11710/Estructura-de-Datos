887776e6df481eea5a14219a8fc62b07917e333f
BryanCr7 <brycordova13@gmail.com>
2025-09-07 00:56:53 -0500

    Subido Práctico Experimental 3 con Program.cs y csproj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semana10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Semana 10: Proyecto listo para compilar."");
+        }
+    }
+}
